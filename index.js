966195b8865f7c28a6aa489592ab6584c8d8418b
omarigg04 <gaxiolaomar04@gmail.com>
2024-07-31 00:48:01 -0600

    testing

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const express = require('express');
-
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const authRoutes = require('./routes/auth');
-
 const postsRoutes = require('./routes/posts');
-
 const errorController = require('./controllers/error');
 
 const app = express();
@@ -14,29 +12,17 @@ const ports = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, PUT, DELETE, OPTIONS',
-    
-  );
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Content-Type, Accept, X-Custom-Header, Authorization'
-  );
-  if (req.method === 'OPTIONS') {
-    return res.status(200).end();
-  }
-  next();
-});
+// Configuración CORS con el paquete cors
+app.use(cors({
+  origin: 'https://social-app-frontend-delta.vercel.app', // Cambia esto a la URL de tu frontend
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Accept', 'X-Custom-Header', 'Authorization']
+}));
 
 app.use('/auth', authRoutes);
-
 app.use('/post', postsRoutes);
 
 app.use(errorController.get404);
-
 app.use(errorController.get500);
 
 app.listen(ports, () => console.log(`Listening on port ${ports}`));
