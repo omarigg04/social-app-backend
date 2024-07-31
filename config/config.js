10298bc120aee65bbc0316c5cd1d78bfe4da2d83
omarigg04 <gaxiolaomar04@gmail.com>
2024-07-30 22:34:53 -0600

    first commit

---
+++ config/config.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+
+const config = {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD
+};
+
+module.exports = config;
